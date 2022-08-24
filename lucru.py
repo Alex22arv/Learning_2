2773fc9eb7af013bd2eb2b7eda357f66da1b9a48
macbookpro <vilcea.alexandru22@gmail.com>
2022-08-24 09:21:23 +0300

    some changes

--- lucru.py
+++ lucru.py
@@ -10,17 +10,28 @@ cars_data = list(cars_data_base_reader)
 # for i in cars_data:
 #     print(i)
 def search_car(cars_data: list):
-    answer1 = int(input("Whitch is the mininum year of manufacture that you prefer? "))
-    answer2 = int(input("What's your budget?"))
-    answer3 = input("What fuel do you prefer?")
-    answer4 = int(input("What is the minimum horsepower you want?"))
+    year= int(input("Whitch is the mininum year of manufacture that you prefer? "))
+    budget = int(input("What's your budget? "))
+    fuel = input("What fuel do you prefer? ")
+    horsepower = int(input("What is the minimum horsepower you want? "))
+    list_of_cars=[]
     for row in cars_data:
-        if answer1 <= int(row[5]) and answer2 >= int(row[4]) and answer3 == row[7] and answer4 <= int(row[8]):
+        if year <= int(row[5]) and budget >= int(row[4]) and fuel == row[7] and horsepower <= int(row[8]):
+            list_of_cars.append([row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]])
             print(f"Your budget includes the next car: {row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]} {row[7]} {row[8]} {row[9]} {row[10]}")
-        elif answer1 <= int(row[5]) and answer3 == row[7] and answer4 <= int(row[8]):
-                print(f"You don't have enough budget for a car but found these matches{row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]} {row[7]} {row[8]} {row[9]} {row[10]}")
 
-answer=input("Welcome,Do you want to buy a car?")
+    if len(list_of_cars)==0:
+           for row in cars_data:
+                if year <= int(row[5]) and budget + 2000 >= int(row[4]) and fuel == row[7] and horsepower <= int(row[8]):
+                    print(f"You don't have enough budget for a car but found these matches : {row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]} {row[7]} {row[8]} {row[9]} {row[10]}")
+                    list_of_cars.append([row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]])
+
+    if len(list_of_cars) == 0:
+         print("Sorry, we don't have any cars for your requests")
+
+
+
+answer=input("Welcome,Do you want to buy a car? ")
 if answer.casefold() == "yes" :
     search_car(cars_data)
 
@@ -28,13 +39,14 @@ elif answer.casefold() == "no" :
     print("thank you for visiting us , see you again")
 else:
      while True :
-         answer5 = input("wrong answer, please try again")
-         if answer5 == "yes" or answer5 == "YES" or answer5 == "Yes":
+         wrong_answer = input("wrong answer, please try again")
+         if wrong_answer.casefold() == "yes" :
             search_car(cars_data)
             break
-         elif answer5 == "no" or answer5 == "NO" or answer5 == "No":
+         elif wrong_answer.casefold() == "no" :
             print("thank you for visiting us , see you again")
             break
+
 
 
 
