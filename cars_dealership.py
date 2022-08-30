b60eb775f477c00e2e64f23d77512cec84909c91
macbookpro <vilcea.alexandru22@gmail.com>
2022-08-30 21:59:10 +0300

    some changes

--- cars_dealership.py
+++ cars_dealership.py
@@ -1,20 +1,22 @@
-
 import csv
-from project_extension import max_value_hp, min_value_consump
+from project_extension import buying_the_car, max_value_hp, min_value_consump
 from pathlib import Path
 
 
 
-cars_db_doc_path = Path("/Users/macbookpro/Desktop/doc.csv")
+
+cars_db_doc_path = Path(r"/Users/macbookpro/Desktop/doc.csv")
 cars_data_base = open(cars_db_doc_path)
 cars_data_base_reader = csv.reader(cars_data_base)
 cars_data = list(cars_data_base_reader)
+cars_data[0][0]='1'
+
 
 
 def search_car(cars_data: list):
     while True:
         try:
-            year= int(input("Whitch is the mininum year of manufacture that you prefer? "))
+            year= int(input("Which is the mininum year of manufacture that you prefer? "))
             break
         except ValueError:
             print("Wrong answer, please try again ")
@@ -47,31 +49,34 @@ def search_car(cars_data: list):
     for row in cars_data:
            if year <= int(row[5]) and budget >= int(row[4]) and fuel == row[7] and horsepower <= int(row[8]):
             list_of_cars.append([row[0],row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]])
-            print(f"Your budget includes the next car: -- {row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]} {row[7]} {row[8]} {row[9]} {row[10]}")
+            print(f"Your budget includes the next car: --{row[0]} {row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]} {row[7]} {row[8]} {row[9]} {row[10]}")
 
 
     if len(list_of_cars)==0:
            for row in cars_data:
                 if year <= int(row[5]) and budget + 2000 >= int(row[4]) and fuel == row[7] and horsepower <= int(row[8]):
-                    print(f"You don't have enough budget for a car but found these matches : -- {row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]} {row[7]} {row[8]} {row[9]} {row[10]}")
+                    print(f"You don't have enough budget for a car but found these matches : --{row[0]} {row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]} {row[7]} {row[8]} {row[9]} {row[10]}")
                     list_of_cars.append([row[0],row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]])
 
     if len(list_of_cars) == 0:
          print("Sorry, we don't have any cars for your requests")
 
+    if len(list_of_cars)>0:
 
+        list_of_maxHpValues=max_value_hp(list_of_cars)
+        top_five_hp_cars=[]
+        for car in list_of_cars:
+            if int(car[8]) in list_of_maxHpValues:
+                top_five_hp_cars.append([car[0],car[1], car[2], car[3], car[4], car[5], car[6], car[7], car[8], car[9], car[10]])
 
-    list_of_maxHpValues=max_value_hp(list_of_cars)
-    top_five_hp_cars=[]
-    for car in list_of_cars:
-        if int(car[8]) in list_of_maxHpValues:
-            top_five_hp_cars.append([car[0],car[1], car[2], car[3], car[4], car[5], car[6], car[7], car[8], car[9], car[10]])
+        min_cons=min_value_consump(top_five_hp_cars)
 
-    min_cons=min_value_consump(top_five_hp_cars)
+        for car in top_five_hp_cars:
+            if float(car[9])==min_cons :
+                print(f'Our top choice/choices : --{car[0]} {car[1]} {car[2]} {car[3]} {car[4]} {car[5]} {car[6]} {car[7]} {car[8]} {car[9]} {car[10]}')
 
-    for car in top_five_hp_cars:
-        if float(car[9])==min_cons :
-            print(f'Our top choice/choices : -- {car[1]} {car[2]} {car[3]} {car[4]} {car[5]} {car[6]} {car[7]} {car[8]} {car[9]} {car[10]}')
+        return list_of_cars
+
 
 
 
@@ -79,7 +84,9 @@ def search_car(cars_data: list):
 if __name__=='__main__':
     answer=input("Welcome to the Audi Dealership , are you interested to buying a car? ")
     if answer.casefold() == "yes" :
-        search_car(cars_data)
+        list_of_sorted_cars=search_car(cars_data)
+        if list_of_sorted_cars!= None:
+            buying_the_car(cars_data, list_of_sorted_cars)
 
     elif answer.casefold() == "no" :
         print("Thank you for visiting us , see you again")
@@ -87,20 +94,10 @@ if __name__=='__main__':
          while True :
              wrong_answer = input("Wrong answer, please try again ")
              if wrong_answer.casefold() == "yes" :
-                search_car(cars_data)
+                list_of_sorted_cars=search_car(cars_data)
+                if list_of_sorted_cars != None:
+                    buying_the_car(cars_data, list_of_sorted_cars)
                 break
              elif wrong_answer.casefold() == "no" :
                 print("Thank you for visiting us , see you again")
                 break
-
-
-
-
-
-
-
-
-
-
-
-
--- project_extension.py
+++ project_extension.py
@@ -1,4 +1,4 @@
-
+import csv
 
 def max_value_hp(list_of_cars):
     list_of_max_value_horsepower = []
@@ -19,3 +19,34 @@ def min_value_consump(list_of_cars):
     list_of_min_value_consumption.sort()
     min_value_consump=list_of_min_value_consumption[0]
     return min_value_consump
+
+def buying_the_car(cars_data,sorted_list):
+    list_of_id = []
+    for car in sorted_list:
+        list_of_id.append(int(car[0]))
+
+    while True:
+        try:
+            answer_interestBuying = input("Are you interested in any of these cars? ")
+            if answer_interestBuying.casefold() == "yes":
+                car_id = int(input("Which is the id of the car you want? "))
+                if car_id in list_of_id:
+                    print("You bought the car . This car is no longer available in our stock ")
+                    for car in cars_data:
+                        if int(car[0]) == car_id:
+                            cars_data.remove(car)
+                    with open("/Users/macbookpro/Desktop/Book3.csv", 'w') as f:
+                        write = csv.writer(f)
+                        write.writerows(cars_data)
+                    break
+                else:
+                    print("We haven't found any cars with this id ")
+                    break
+            elif answer_interestBuying.casefold() == "no":
+                print("Thank you for visiting us , see you again")
+                break
+            print('Wrong answer, please try again')
+
+        except ValueError:
+            print('Wrong answer, please try again')
+
